98c7a90a8cd0bfeead065400d5814633cef789e5
Dalitso Gore <youngannkavell@gmail.com>
2024-02-14 11:19:50 -0800

    Turtle Racing

---
+++ turtleracinggame.py
@@ -0,0 +1,16 @@
+# Author: Dalitso Gore
+# Date:     2/14/24
+# Description : Turle Racing Game with Turtle
+# Revision History
+# Name      Date       Desc
+# scl        2/12/24   Initial code base
+
+import time
+import turtle
+import random as rd
+
+start_x = -200
+start_y = 150
+
+wn = turtle.Screen()
+wn.bgcolor("red")
